354ed181d54c00caa77be72a1c0c17d56c3a8de3
Anton Reinhard <s1509337@msx.tu-dresden.de>
2016-12-03 01:42:57 +0100

    Containing main()-function, starting point

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
+#include "game.h"
+#include "SDL_setup.h"
+
+int main( int argc, char* args[] )
+{
+	initGraphics();
+
+	return 0;
+}
