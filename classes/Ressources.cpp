ed3cda3bdc0b11c07d4779bc8f7c3b07298f7a50
Anton Reinhard <s1509337@msx.tu-dresden.de>
2017-04-02 17:27:07 +0200

    Ressources Class finished

--- classes/Ressources.cpp
+++ classes/Ressources.cpp
@@ -4,9 +4,94 @@
 
 Ressources::Ressources()
 {
+	mGold = 0;
+	mWood = 0;
+	mStone = 0;
+	mIron = 0;
+	mEnergy = 0;
+	mWater = 0;
+	mFood = 0;
 }
 
 
-Ressources::~Ressources()
+Ressources::Ressources(int gold, int wood, int stone, int iron, int energy, int water, int food)
 {
+	mGold = gold;
+	mWood = wood;
+	mStone = stone;
+	mIron = iron;
+	mEnergy = energy;
+	mWater = water;
+	mFood = food;
 }
+
+void Ressources::set_gold(int gold)
+{
+	mGold = gold;
+}
+
+int Ressources::get_gold() const
+{
+	return mGold;
+}
+
+void Ressources::set_wood(int wood)
+{
+	mWood = wood;
+}
+
+int Ressources::get_wood() const
+{
+	return mWood;
+}
+
+void Ressources::set_stone(int stone)
+{
+	mStone = stone;
+}
+
+int Ressources::get_stone() const
+{
+	return mStone;
+}
+
+void Ressources::set_iron(int iron)
+{
+	mIron = iron;
+}
+
+int Ressources::get_iron() const
+{
+	return mIron;
+}
+
+void Ressources::set_energy(int energy)
+{
+	mEnergy = energy;
+}
+
+int Ressources::get_energy() const
+{
+	return mEnergy;
+}
+
+void Ressources::set_water(int water)
+{
+	mWater = water;
+}
+
+int Ressources::get_water() const
+{
+	return mWater;
+}
+
+void Ressources::set_food(int food)
+{
+	mFood = food;
+}
+
+int Ressources::get_food() const
+{
+	return mFood;
+}
+
--- headers/Ressources.h
+++ headers/Ressources.h
@@ -3,6 +3,30 @@ class Ressources
 {
 public:
 	Ressources();
-	~Ressources();
-};
+	Ressources(int gold, int wood, int stone, int iron, int energy, int water, int food);
 
+	//set and get all the different values
+	void set_gold(int gold);
+	int get_gold() const;
+	void set_wood(int wood);
+	int get_wood() const;
+	void set_stone(int stone);
+	int get_stone() const;
+	void set_iron(int iron);
+	int get_iron() const;
+	void set_energy(int energy);
+	int get_energy() const;
+	void set_water(int water);
+	int get_water() const;
+	void set_food(int food);
+	int get_food() const;
+
+private:
+	int mGold;
+	int mWood;
+	int mStone;
+	int mIron;
+	int mEnergy;
+	int mWater;
+	int mFood;
+};
