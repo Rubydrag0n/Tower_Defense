b8eaa7a2f795a65bec41215ee80cc4f26cebc918
Anton Reinhard <s1509337@msx.tu-dresden.de>
2017-04-03 09:08:15 +0200

    Damage Calculation added

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include <stdio.h>
 //#include "game.h"
 #include "SDL_setup.h"
+#include "SDL_config.h"
 
 int main( int argc, char* args[] )
 {
 	initGraphics();
+	
+	//CFG_File config;
 
 	return 0;
 }
