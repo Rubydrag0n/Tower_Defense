76d38a7de978b7bdc07fc6cd3abaa7a68b4b0045
habanzu <habanzu@gmail.com>
2017-05-20 00:56:14 +0200

    Added a abstract class.

--- classes/Building.cpp
+++ classes/Building.cpp
@@ -2,11 +2,41 @@
 
 
 
-Building::Building()
+Building::Building(SDL_Point coords)
 {
+	set_coords(coords);
 }
-
 
 Building::~Building()
 {
+}
+
+SDL_Point Building::get_coords() const
+{
+	return mCoords;
+}
+
+Ressources Building::get_construction_costs() const
+{
+	return mConstruction_costs;
+}
+
+Ressources Building::get_maintenance() const
+{
+	return mMaintenance;
+}
+
+void Building::set_coords(SDL_Point coords)
+{
+	mCoords = coords;
+}
+
+void Building::set_construction_costs(Ressources cost)
+{
+	mConstruction_costs = cost;
+}
+
+void Building::set_maintenance(Ressources new_maintenance)
+{
+	mMaintenance = new_maintenance;
 }
--- headers/Building.h
+++ headers/Building.h
@@ -1,7 +1,27 @@
 #pragma once
+#include <vector>
+#include "Ressources.h"
+#include "SDL.h"
+
 class Building
 {
 public:
-	Building();
+	Building(SDL_Point coords);
 	~Building();
+	
+	virtual void update();
+
+	SDL_Point get_coords() const;
+	Ressources get_construction_costs() const;
+	Ressources get_maintenance() const;
+	void set_coords(SDL_Point coords);
+	void set_construction_costs(Ressources costs);
+	void set_maintenance(Ressources maintenance);
+
+
+private:
+	SDL_Point mCoords;
+	Ressources mConstruction_costs;
+	Ressources mMaintenance;
+
 };
