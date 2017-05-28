3f0117f79f90aa5c88400eb062b7f5b0e5a4a818
Anton Reinhard <s1509337@msx.tu-dresden.de>
2017-05-28 22:48:34 +0200

    Windows are working, no background yet

---
+++ classes/Window.cpp
@@ -0,0 +1,82 @@
+#include "Window.h"
+#include "SDL_setup.h"
+#include "ConfigFile.h"
+
+Window::Window(SDL_Rect dim, STYLE style)
+{
+	ConfigFile cf("config/game.cfg");
+	mDim = dim;
+	mStyle = style;
+
+	std::string category = "frames/" + std::to_string(style);
+	mBlcorner = gTextures->get_texture(cf.Value(category, "blcpath"));
+	mBrcorner = gTextures->get_texture(cf.Value(category, "brcpath"));
+	mTlcorner = gTextures->get_texture(cf.Value(category, "tlcpath"));
+	mTrcorner = gTextures->get_texture(cf.Value(category, "trcpath"));
+	mHorizontalborder = gTextures->get_texture(cf.Value(category, "hbpath"));
+	mVerticalborder = gTextures->get_texture(cf.Value(category, "vbpath"));
+
+	mCorner_height = cf.Value(category, "corner_height");
+	mCorner_width = cf.Value(category, "corner_width");
+	mBorder_thickness = cf.Value(category, "border_thickness");
+}
+
+Window::~Window()
+{
+	//Don't destroy textures here
+}
+
+void Window::render() const
+{
+	//TODO: Draw the background first
+	
+	SDL_Rect dest;
+	dest.w = mCorner_width;
+	dest.h = mCorner_height;
+
+	//draw the four corners
+	dest.x = mDim.x;
+	dest.y = mDim.y;
+	SDL_RenderCopy(gRenderer, mTlcorner, nullptr, &dest);
+	dest.x = mDim.x + mDim.w - mCorner_width;
+	SDL_RenderCopy(gRenderer, mTrcorner, nullptr, &dest);
+	dest.y = mDim.y + mDim.h - mCorner_height;
+	SDL_RenderCopy(gRenderer, mBrcorner, nullptr, &dest);
+	dest.x = mDim.x;
+	SDL_RenderCopy(gRenderer, mBlcorner, nullptr, &dest);
+
+	//draw the borders
+	//horizontal top:
+	dest.y = mDim.y;
+	dest.w = 1;
+	dest.h = mBorder_thickness;
+	for (dest.x = mDim.x + mCorner_width; dest.x < mDim.x + mDim.w - mCorner_width; dest.x++)
+	{
+		SDL_RenderCopy(gRenderer, mHorizontalborder, nullptr, &dest);
+	}
+	//horizontal bottom:
+	dest.y = mDim.y + mDim.h - mBorder_thickness;
+	for (dest.x = mDim.x + mCorner_width; dest.x < mDim.x + mDim.w - mCorner_width; dest.x++)
+	{
+		SDL_RenderCopy(gRenderer, mHorizontalborder, nullptr, &dest);
+	}
+	//vertical left:
+	dest.x = mDim.x;
+	dest.w = mBorder_thickness;
+	dest.h = 1;
+	for (dest.y = mDim.y + mCorner_height; dest.y < mDim.y + mDim.h - mCorner_height; dest.y++)
+	{
+		SDL_RenderCopy(gRenderer, mVerticalborder, nullptr, &dest);
+	}
+	//vertical right
+	dest.x = mDim.x + mDim.w - mBorder_thickness;
+	for (dest.y = mDim.y + mCorner_height; dest.y < mDim.y + mDim.h - mCorner_height; dest.y++)
+	{
+		SDL_RenderCopy(gRenderer, mVerticalborder, nullptr, &dest);
+	}
+}
+
+void Window::set_dim(SDL_Rect dim)
+{
+	this->mDim = dim;
+}
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "Map.h"
 #include "Unit.h"
 #include "MonsterGroup.h"
+#include "Window.h"
 
 int main( int argc, char* args[] )
 {
@@ -15,6 +16,21 @@ int main( int argc, char* args[] )
 	else
 	{
 		auto test = new Map("level/Level1.FMP");
+
+		const int winc = 10;
+
+		SDL_Rect d[winc];
+		for (auto i = 0; i < winc; i++) {
+			d[i].x = rand() % 300 + 50;
+			d[i].y = rand() % 300 + 50;
+			d[i].w = rand() % 1000 + 50;
+			d[i].h = rand() % 600 + 50;
+		}
+		Window *windows[winc];
+		for (auto i = 0; i < winc; i++)
+		{
+			windows[i] = new Window(d[i]);
+		}
 
 		SDL_RenderClear(gRenderer);
 
@@ -30,12 +46,30 @@ int main( int argc, char* args[] )
 			SDL_RenderClear(gRenderer);
 			test->render();
 			testMonsterGroup->render();
+
+			for (auto j = 0; j < winc; j++)
+			{
+				d[j].x += rand() % 9 - 4;
+				d[j].y += rand() % 9 - 4;
+				d[j].w += rand() % 9 - 4;
+				if (d[j].w < 150) d[j].w = 150;
+				d[j].h += rand() % 9 - 4;
+				if (d[j].h < 150) d[j].h = 150;
+
+				windows[j]->set_dim(d[j]);
+
+				windows[j]->render();
+			}
 			SDL_RenderPresent(gRenderer);
 			if (i % 60 == 0) printf("Second...\n");
 			if (testMonsterGroup->isDead()) break;
 		}
 		delete testMonsterGroup;
 		delete test;
+		for (auto i = 0; i < winc; i++)
+		{
+			delete windows[i];
+		}
 	}
 	
 	SDL_Delay(1000);
