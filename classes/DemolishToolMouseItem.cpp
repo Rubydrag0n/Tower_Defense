f75db695ab23751e939c993fd3194de92289ed71
„Quantor <jensch.oliver@web.de>
2019-04-26 14:08:16 +0200

    mouse_over demolish

--- classes/DemolishToolMouseItem.cpp
+++ classes/DemolishToolMouseItem.cpp
@@ -23,3 +23,17 @@ void DemolishToolMouseItem::on_click(int mouse_x, int mouse_y)
 	}
 }
 
+void DemolishToolMouseItem::on_mouse_over(int mouse_x, int mouse_y)
+{
+	if (mouse_x < 1280 && this->get_state() == MOUSE_DOWN_LEFT)
+	{
+		const auto tile_x = mouse_x / TILE_WIDTH;
+		const auto tile_y = mouse_y / TILE_HEIGHT;
+		auto building = mLevel->get_building_matrix(tile_x, tile_y);
+		if (building != nullptr)
+		{
+			building->demolish();
+			delete building;
+		}
+	}
+}
