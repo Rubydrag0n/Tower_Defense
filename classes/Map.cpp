69cbc4600852303faf6ba235f26ab9eae14e66fb
Rubydrag0n <s1509337@msx.tu-dresden.de>
2020-01-26 20:28:46 +0100

    Use protobuf

--- classes/Map.cpp
+++ classes/Map.cpp
@@ -1,11 +1,13 @@
 #include "Map.h"
 #include "ConfigFile.h"
 #include "LayerHandler.h"
-#include <fstream>
-#include <sstream>
 #include "SDL_setup.h"
 #include <SDL.h>
+#include <google/protobuf/util/json_util.h>
+
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 Map::Map() : Renderable(BACKGROUND)
 {
@@ -49,9 +51,9 @@ Map::~Map()
 	mMap_texture->free();
 	delete mMap_texture;
 
-	for (auto &path : mTile_paths)
+	for (auto& pair : mMap.tile_map())
 	{
-		gTextures->del_texture(path);
+		gTextures->del_texture(pair.second);
 	}
 }
 
@@ -67,88 +69,20 @@ int Map::get_width() const
 
 bool Map::deserialize(std::string& path)
 {
-	const auto tiles_x = 20;
-	const auto tiles_y = 16;
-
 	std::ifstream file(path);
 	if (!file.is_open())
 	{
+		std::cerr << "Couldn't read map from " << path << "." << std::endl;
 		return false;
 	}
 
-	std::string content;
+	std::stringstream json_string;
 
-	//reading tile sizes
-	std::getline(file, content);
-	this->mTile_size_x = std::stoi(content);
-	std::getline(file, content);
-	this->mTile_size_y = std::stoi(content);
+	json_string << file.rdbuf();
 
-	//reading the tiles
-	std::getline(file, content);
-	while (content != "endtiles")
-	{
-		char tile_char;
-		std::string tile_path;
-		std::stringstream ss;
-		ss << content;
-		ss >> tile_char;
-		ss >> tile_path;
+	auto status = google::protobuf::util::JsonStringToMessage(json_string.str(), &mMap);
 
-		//TODO: Error handling
-		auto tile_texture = gTextures->get_texture(tile_path);
-
-		if (tile_texture == nullptr)
-		{
-			std::cout << "Couldn't load tile texture at " << tile_path << std::endl;
-		}
-
-		mTiles.insert(std::pair<char, LTexture*>(tile_char, tile_texture));
-		mTile_paths.push_back(tile_path);
-
-		std::getline(file, content);
-	}
-
-   	const auto map_width = mTile_size_x * tiles_x;
-	const auto map_height = mTile_size_y * tiles_y;
-
-	if (!mMap_texture->create_blank(map_width, map_height, SDL_TEXTUREACCESS_TARGET))
-	{
-		printf("Failed to create target texture!\n");
-		file.close();
-		return false;
-	}
-
-	//we want to render to the texture
-	mMap_texture->set_as_render_target();
-
-	SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-	SDL_RenderClear(gRenderer);
-
-	SDL_Rect dest{ 0, 0, mTile_size_x, mTile_size_y };
-
-	//read the background and render it to the map texture
-	for (auto y = 0; y < tiles_y; ++y)
-	{
-		std::getline(file, content);
-		char tile;
-		std::stringstream ss;
-		ss << content;
-		
-		for (auto x = 0; x < tiles_x; ++x)
-		{
-			ss >> tile;
-			mTiles[tile]->render(&dest);
-			dest.x += dest.w;
-		}
-
-		dest.x = 0;
-		dest.y += dest.h;
-	}
-
-	file.close();
-
-	return true;
+	return status.ok();
 }
 
 void Map::render()
--- headers/Map.h
+++ headers/Map.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "LTexture.h"
 #include "Renderable.h"
+#include "../proto/map.pb.h"
+
 #include <vector>
 #include <map>
 
@@ -24,6 +26,9 @@ public:
 protected:
 	void update_map_texture() const;
 
+	//the map data structure
+	towerdefense::map mMap;
+
 	int mOffset_left;
 	int mOffset_top;
 	int mWidth;
@@ -33,14 +38,11 @@ protected:
 	//members that are deserialized from the map file
 	int mTile_size_x;
 	int mTile_size_y;
-
-	//a map containing the characters and their graphics
-	std::map<char, LTexture*> mTiles;
-
-	//a vector storing the paths to the tile textures so they can be deleted in the destructor
-	std::vector<std::string> mTile_paths;
 	
-	//the finished texture, consisting of the backgroundlayer with added decorations
+	//the finished texture, consisting of the background layer with added decorations
 	LTexture *mMap_texture;
 	LTexture *mBackground_texture;
+
+private:
+
 };
