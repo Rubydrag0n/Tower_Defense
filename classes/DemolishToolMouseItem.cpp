17080332af5a22cc9f92fbcb37595f9447ec3994
Rubydrag0n <s1509337@msx.tu-dresden.de>
2020-04-08 21:02:43 +0200

    Fix not being able to switch menu tabs while holding something

--- classes/DemolishToolMouseItem.cpp
+++ classes/DemolishToolMouseItem.cpp
@@ -2,30 +2,27 @@
 #include "SDL_setup.h"
 #include "Building.h"
 
-DemolishToolMouseItem::DemolishToolMouseItem(const std::string& name_of_object, LTexture* sprite, Level* level, const LAYERS click_layer) 
-											: MouseItem(name_of_object, sprite, level, click_layer)
+DemolishToolMouseItem::DemolishToolMouseItem(const std::string& name_of_object, LTexture* sprite, Level* level, const LAYERS click_layer)
+	: MouseItem(name_of_object, sprite, level, click_layer)
 {
-	
+
 }
 
 void DemolishToolMouseItem::on_click(const int mouse_x, const int mouse_y)
 {
-	if (mouse_x < 1280)
+	const auto tile_x = mouse_x / TILE_WIDTH;
+	const auto tile_y = mouse_y / TILE_HEIGHT;
+	const auto building = mLevel->get_building_matrix(tile_x, tile_y);
+	if (building != nullptr)
 	{
-		const auto tile_x = mouse_x / TILE_WIDTH;
-		const auto tile_y = mouse_y / TILE_HEIGHT;
-		const auto building = mLevel->get_building_matrix(tile_x, tile_y);
-		if(building != nullptr)
-		{
-			building->demolish();
-			delete building;
-		}
+		building->demolish();
+		delete building;
 	}
 }
 
 void DemolishToolMouseItem::on_mouse_over(const int mouse_x, const int mouse_y)
 {
-	if (mouse_x < 1280 && this->get_state() == MOUSE_DOWN_LEFT)
+	if (this->get_state() == MOUSE_DOWN_LEFT)
 	{
 		const auto tile_x = mouse_x / TILE_WIDTH;
 		const auto tile_y = mouse_y / TILE_HEIGHT;
